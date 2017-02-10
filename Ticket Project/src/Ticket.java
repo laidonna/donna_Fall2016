627ee23aad02a5f8f29060cd5a23a9b4d6b872f4
laidonna <lai.donna@outlook.com>
2017-02-09 21:11:35 -0800

    added to walkupTicket

--- Ticket Project/src/Ticket.java
+++ Ticket Project/src/Ticket.java
@@ -1,8 +1,17 @@
 abstract class Ticket {
 	private int number;
-	private double getPrice(){}
-	private void toString(){}
+	public double price = 50.0;
+	
+	private double getPrice(){
+		return this.price;
+	}
+	
+	public String toString(){
+		return "Number: " + number + " Price: " + getPrice();
+	}
+	
 	public Ticket(int number){
+		number = this.number;
 		double price = getPrice();
 	}
 }
--- Ticket Project/src/WalkupTicket.java
+++ Ticket Project/src/WalkupTicket.java
@@ -1,9 +1,8 @@
 
 public class WalkupTicket extends Ticket{
 
-	public WalkupTicket(int number) {
-		super(number);
-		// TODO Auto-generated constructor stub
+	public WalkupTicket(int number, double price) {
+		int num = number;
+		price = super.price;
 	}
-
 }
