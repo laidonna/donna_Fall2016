26d7864f577e65a8a94d8643d672d149e1c06ccc
laidonna <lai.donna@outlook.com>
2017-02-22 22:14:55 -0800

    File Temp Diff
    FINALLY FINISHED

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/96/00d5425c81f9001618cda996900ba895
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class TempFileDiff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner console = new Scanner(new File("weather.txt"));
+		double num = console.nextDouble();
+		while (console.hasNextDouble()){
+			System
+		}
+	}
+
+}
--- Temperature Difference/src/TempFileDiff.java
+++ Temperature Difference/src/TempFileDiff.java
@@ -1,14 +1,39 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class TempFileDiff {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner console = new Scanner(new File("weather.txt"));
-		double num = console.nextDouble();
-		while (console.hasNextDouble()){
-			System
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner console = new Scanner(new File("src/weather.txt"));		
+		double change;
+		double first, next;
+		first = console.nextDouble();
+		next = first;
+		while (console.hasNextDouble()){ 
+			first = console.nextDouble(); 
+			change = first - next;
+			System.out.println(next + " to " + first + ", change = " + round2(change));
+			next = first;
 		}
 	}
+	
+	public static double round2(double num){
+		double xBy100 = num * 100.0;
+		double xhalf;
+		double answer;
+		if (num > 0){
+			xhalf = xBy100 + 0.5;
+			xhalf = (int) xhalf;
+			answer = xhalf / 100.0;
+			return answer;
+		}else{
+			xhalf = xBy100 - 0.5;
+			xhalf = (int) xhalf;
+			answer = xhalf / 100.0;
+			return answer;
+		}
 
+	}
 }
+
+
