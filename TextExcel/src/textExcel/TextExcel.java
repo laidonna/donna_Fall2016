8378398705a044b624cc4186dfbdd0960880c470
laidonna <lai.donna@outlook.com>
2017-03-03 08:45:22 -0800

    more ew

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -9,8 +9,16 @@ import java.util.Scanner;
 public class TextExcel{
 
 	public static void main(String[] args){
+		Spreadsheet sheet = new Spreadsheet();
 	    Scanner input = new Scanner(System.in);
+	    System.out.println("Input yo stuff");
 	    String cellDeclaration = input.nextLine();
-	    System.out.println(Spreadsheet.processCommand(cellDeclaration));
+	    while(!input.equals("quit")){
+	    	String cellDeclaration = input.nextLine();
+	 	    System.out.println(Spreadsheet.processCommand(cellDeclaration));
+		    System.out.println("Input yo stuff");
+		    cellDeclaration = input.nextLine();
+	    }
+	   
 	}
 }
